50325767dc24483a40e08a6c2ea1afb02535cb26
Renann Lima <renann@unix.com.br>
2026-02-01 17:56:41 -0300

    git commit -m

---
+++ airbnb/models/src/src_hosts.sql
@@ -0,0 +1,11 @@
+WITH raw_hosts AS (
+select * from {{ source('airbnb', 'hosts')}}
+)
+SELECT
+    id AS host_id,
+    NAME AS host_name,
+    is_superhost,
+    created_at,
+    updated_at
+FROM
+    raw_hosts
